4cc1b9ba0d5411b8a74a7b5871f74d90ed4bc7bf
MityaHl <mitya-hl@mail.ru>
2018-11-28 14:38:14 +0300

    fourCommit

--- public/js/jsfile.js
+++ public/js/jsfile.js
@@ -9,8 +9,8 @@ var myTeamBtn = document.getElementById('myteam-btn'),
     transfers = document.getElementById('transfers'),
     circles = document.querySelectorAll('.oblack'),
     choosePlayer = document.getElementById('choose-player'),
-    table = document.getElementById('table'),
-    chooseBtn = document.getElementById('choose-pl');
+    table = document.getElementById('courttable'),
+    findPl = document.getElementById('find-pl');
 
 function changeClassList(divDisplay, divNoneDisplay1, divNoneDisplay2, divNoneDisplay3){
     divDisplay.classList.add('display');
@@ -24,11 +24,11 @@ function changeClassList(divDisplay, divNoneDisplay1, divNoneDisplay2, divNoneDi
 }
 
 var stadiumWindow = function(){
-    changeClassList(table, choosePlayer);
+    changeClassList(table, choosePlayer, results, transfers);
 }
 
 var choosePlDivWindow = function(){
-    changeClassList(choosePlayer, table);
+    changeClassList(choosePlayer, courttable, results, transfers);
 }
 
 var statsWindow = function(){
@@ -52,10 +52,14 @@ statsBtn.addEventListener('click', statsWindow);
 myTeamBtn.addEventListener('click', myTeamWindow);
 resultBtn.addEventListener('click', resultWindow);
 transfersBtn.addEventListener('click', transfersWindow);
+findPl.addEventListener('click', function (e){
+    let table = document.getElementById('player-table');
+    table.classList.remove('nonedisplay');
+    table.classList.add('display');
+})
 for(var i = 0; i < circles.length; i++){
     circles[i].addEventListener('click', choosePlDivWindow);
 }
-chooseBtn.addEventListener('click', stadiumWindow);
 
 function getUserInfo() {
     $.ajax({
@@ -65,7 +69,7 @@ function getUserInfo() {
             const loginView = document.getElementById("loginName");
             const balanceView = document.getElementById("balance");
             loginView.innerHTML = data["login"];
-            balanceView.innerHTML = data["balance"];
+            balanceView.innerHTML = data["balance"] + ' $';
         }
     })
 }
@@ -88,23 +92,87 @@ function logout() {
     document.cookie = 'Auth-Token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
-function getPlayers() {
+function getPlayers(position, maxCost) {
+    let json = '{ "token": "' + getCookie("Auth-Token") + '"position": "' + position + '" ,"maxCost":"' + maxCost + '"}';
+    delAll();
     $.ajax({
-        url: 'http://localhost:8080' + '/users?token=' + getCookie("Auth-Token"),
-        type: 'get',
+        url: 'http://localhost:8080/getPlayersToBuy',
+        type: 'post',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: json,
         success: function (data, textStatus, request) {
             const table = document.getElementById("player-table");
             for (let i = 0; i < data.length; i++) {
                 let str = "player" + i;
-                let row = table.insertRow(i + 2);
+                let row = table.insertRow(i + 1);
                 row.id = str;
+
                 const cellPosition = row.insertCell(0);
                 const cellCost = row.insertCell(1);
-                const cellButton = row.insertCell(2);
-                cellPosition.innerHTML = data[i]["position"];
-                cellCost.innerHTML = data[i]["maxCost"];
-                cellButton.innerHTML = "<button>Купить</button>>"
+
+                row.onclick = function(){
+                    return buyPlayer(str)
+                };
+
+                cellPosition.innerHTML = data[i]["name"];
+                cellCost.innerHTML = data[i]["cost"];
             }
         }
     })
+}
+
+function buyPlayer(row_id) {
+    var row = document.getElementById(row_id);
+    var playerName = row.cells[0].innerHTML;
+    var json = '{ "token": "' + getCookie("Auth-Token") + '" ,"name":"' + playerName + '"}';
+    $.ajax({
+        url: 'http://localhost:8080/buyPlayer?token=' + getCookie("Auth-Token") + '&name=' + playerName,
+        type: 'get',
+        statusCode: {
+            500: function() {
+                alert("Недостаточно средств");
+            },
+            200: function () {
+                alert("Игрок куплен");
+                window.location = '/index.html';
+            }
+        }
+    })
+}
+
+function getAvailablePlayers(position) {
+    var json = '{ "token": "' + getCookie("Auth-Token") + '" ,"position":"' + position + '"}';
+    $.ajax({
+        url: 'http://localhost:8080/getAvailablePlayers?token=' + getCookie("Auth-Token") + '&position=' + position,
+        type: 'get',
+        success: function (data, textStatus, request) {
+            const table = document.getElementById("available-player-table");
+            for (let i = 0; i < data.length; i++) {
+                let str = "player" + i;
+                let row = table.insertRow(i + 1);
+                row.id = str;
+                const cellName = row.insertCell(0);
+
+                row.onclick = function(){
+                    return setPlayer(str)
+                };
+
+                cellName.innerHTML = data[i];
+            }
+        }
+    })
+}
+
+
+function deleteRow(r, tableId) {
+    let i = r.parentNode.parentNode.rowIndex;
+    document.getElementById(tableId).deleteRow(i);
+}
+
+function delAll(tableId) {
+    let table = document.getElementById(tableId);
+    for(let i = table.rows.length - 1; i > 0; i--){
+        table.deleteRow(i, tableId);
+    }
 }
